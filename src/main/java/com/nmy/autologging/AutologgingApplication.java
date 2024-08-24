32fd1f0f7688649cf23575c49e5e09ca6aa1e3f3
nmy64 <nmy6452@naver.com>
2024-08-24 15:57:05 +0900

    FEAT: 최초 커밋

---
+++ src/main/java/com/nmy/autologging/AutologgingApplication.java
@@ -0,0 +1,13 @@
+package com.nmy.autologging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutologgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutologgingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmy/autologging/AutologgingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmy.autologging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutologgingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
